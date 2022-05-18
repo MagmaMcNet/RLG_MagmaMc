0e978932ba5b613dbec8018fbe8e43775304e25e
SMLkaiellis08 <kaicycle11@gmail.com>
2022-05-18 19:56:27 +0100

    1.1.4

--- _Compile_/Compile.bat
+++
@@ -1,5 +0,0 @@
-cd ../
-del dist\*.zip /Q
-timeout /t 1 >nul
-"C:\Program Files\7-Zip\7z.exe" a "dist/Build_Windows.zip" "dist/"
-timeout /t 2
--- gitpush.sh
+++ gitpush.sh
@@ -1,4 +1,4 @@
 git add .
-git commit -m "1.1.3" -a 
+git commit -m "1.1.4" -a 
 git push -u origin master
-gh release create v1.1.3 --generate-notes 'dist/Build_Windows.zip' --P
+gh release create v1.1.4 --generate-notes 'dist/Build_Windows.zip' -p
