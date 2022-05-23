e93312c450fe70a185237112d9a374add40f0f80
SMLkaiellis08 <kaicycle11@gmail.com>
2022-05-23 18:59:48 +0100

    1.2.0

--- gitpush.sh
+++ gitpush.sh
@@ -1,4 +1,4 @@
 git add .
-git commit -m "1.1.6" -a 
+git commit -m "1.2.0" -a 
 git push -u origin master
-gh release create v1.1.6 --generate-notes 'dist/Build_Windows.zip' -p
+gh release create v1.2.0 --generate-notes 'dist/Build_Windows.zip'
